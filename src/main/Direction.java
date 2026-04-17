d408a66e09a848a73d6a8a9e61cb8a7e8c04bdce
NikitaE36 <zaxody2626@gmail.com>
2026-04-17 12:09:47 +0300

    Delete src directory

--- src/main/Direction.java
+++
@@ -1,5 +0,0 @@
-package main;
-
-public enum Direction {
-    NORTH, SOUTH, EAST, WEST
-}
--- src/main/plant/Plant.java
+++
@@ -1,9 +0,0 @@
-package main.plant;
-
-public class Plant {
-    private double weight = 1;
-    private int maxPerCell = 200;
-
-    public double getWeight() { return weight; }
-    public int getMaxPerCell() { return maxPerCell; }
-}
